8726032fd7eb1ee56296ebb11119bad3bb1610d8
Suddip Paul Arnab <124586591+sudoingcoding@users.noreply.github.com>
2024-02-05 12:16:00 +0600

    Create 387.cpp

---
+++ code/387.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<int,int>m;
+        int r=-1;
+        for(int i=0;i<s.size();i++){
+            m[s[i]]
+        }
+        return r;
+    }
+};
